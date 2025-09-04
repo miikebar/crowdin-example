efe955d0fc1b42f2e0af9c12c4fd8a592d6336ae
Michał Bar <9134970+miikebar@users.noreply.github.com>
2025-09-04 17:05:29 +0200

    Setup i18n with Native

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from "expo-router";
+import i18n from "i18next";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import enUS from "../assets/locales/en-US.json";
+import plPL from "../assets/locales/pl-PL.json";
+
+i18n.use(initReactI18next).init({
+  lng: "en-US",
+  fallbackLng: "en-US",
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    "en-US": {
+      translation: enUS,
+    },
+    "pl-PL": {
+      translation: plPL,
+    },
+  },
+  load: "currentOnly",
+});
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Stack />
+    </I18nextProvider>
+  );
 }
--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,5 +1,18 @@
-import { Text } from "react-native";
+import { getI18n, useTranslation } from "react-i18next";
+import { Pressable, Text, View } from "react-native";
 
 export default function IndexScreen() {
-  return <Text>Hello</Text>;
+  const { t } = useTranslation();
+
+  return (
+    <View>
+      <Text>{t("header")}</Text>
+      <Pressable onPress={() => getI18n().changeLanguage("pl-PL")}>
+        <Text>Change to PL</Text>
+      </Pressable>
+      <Pressable onPress={() => getI18n().changeLanguage("en-US")}>
+        <Text>Change to EN</Text>
+      </Pressable>
+    </View>
+  );
 }
